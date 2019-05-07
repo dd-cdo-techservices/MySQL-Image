fe8baa2367c494f80a3d1b417e70a24a7f5debce
Shivayogi Kamat <yogisdmec007@gmail.com>
2019-05-07 11:22:52 +0530

    MySQL IMage build

---
+++ mysql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+################################################################################
+# Start of Packer
+################################################################################
+
+#
+#Installing MySQL 5.7 which is available in default repo for Ubuntu 16.06
+#
+
+echo "Installing MySQL 5.7 now...\n"
+
+sudo echo "mysql-server-5.7 mysql-server/root_password password Password@123" | sudo debconf-set-selections
+sudo echo "mysql-server-5.7 mysql-server/root_password_again password Password@123" | sudo debconf-set-selections
+sudo apt-get -y install mysql-server-5.7 mysql-client
+
+#mysql -u root -p root -e "use mysql; UPDATE user SET authentication_string=PASSWORD('$MYSQLPASSWORD') WHERE User='root'; flush privileges;" >> $LOGFILE 2>&1
+
+
+echo "Creating Wordpress DB Users and grating privileges with already collected information...\n"
+
+sudo mysqladmin -u root -pPassword@123 create blog
+
+################################################################################
+# End of Packer
+################################################################################
