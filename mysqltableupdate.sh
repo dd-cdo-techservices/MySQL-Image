2a860a96eef99ac9beb8f23dde250808e5e20c7e
yogisdm <31575108+yogisdm@users.noreply.github.com>
2019-05-08 14:01:20 +0530

    code update

--- mysqltableupdate.sh
+++ mysqltableupdate.sh
@@ -1,2 +1,2 @@
-mysql -u root -D mysql -pPassword@123 -e "UPDATE user SET Host='10.0.2.104' WHERE Host='localhost' AND User='root'"
+mysql -u root -D mysql -pPassword@123 -e "use mysql;UPDATE user SET Host='10.0.2.104' WHERE Host='localhost' AND User='root';"
 sudo systemctl restart mysql
